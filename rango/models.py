6a25ad674898f063d4ed0452ab33c686df93f715
brodieWest <brodie.west3@gmail.com>
2017-01-27 17:35:55 +0000

    6.3 not working

--- rango/models.py
+++ rango/models.py
@@ -1,16 +1,21 @@
 from __future__ import unicode_literals
-
+from django.template.defaultfilters import slugify
 from django.db import models
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    likes = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
-        
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
