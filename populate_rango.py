834b92130449d4a2da47cd65f9c5cc8ac52ef9cb
brodieWest <brodie.west3@gmail.com>
2017-01-27 16:12:41 +0000

    new db

--- populate_rango.py
+++ populate_rango.py
@@ -35,9 +35,9 @@ def populate():
          {"title":"Flask",
           "url":"http://flask.pocoo.org"} ]
      
-     cats = {"Python": {"pages": python_pages},
-             "Django": {"pages": django_pages},
-             "Other Frameworks": {"pages": other_pages} }
+     cats = {"Python": {"pages": python_pages, "likes": 64, "views": 128},
+             "Django": {"pages": django_pages, "likes": 32, "views": 64},
+             "Other Frameworks": {"pages": other_pages, "likes": 16, "views": 32} }
     
      # If you want to add more catergories or pages,
      # add them to the dictionaries above.
@@ -49,7 +49,7 @@ def populate():
      # for more information about how to iterate over a dictionary properly.
      
      for cat, cat_data in cats.items():
-         c = add_cat(cat)
+         c = add_cat(cat, cat_data["likes"], cat_data["views"])
          for p in cat_data["pages"]:
              add_page(c, p["title"], p["url"])
      
@@ -65,8 +65,10 @@ def add_page(cat, title, url, views=0):
      p.save()
      return p
  
-def add_cat(name):
+def add_cat(name,likes,views):
      c = Category.objects.get_or_create(name=name)[0]
+     c.likes = likes
+     c.views = views
      c.save()
      return c
  
