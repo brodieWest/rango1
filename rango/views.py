708168286c8f77de63a929e79e6a6b3419d8ce53
brodieWest <brodie.west3@gmail.com>
2017-02-09 17:51:34 +0000

    10 finishedgit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -Agit add -A >:D

--- rango/views.py
+++ rango/views.py
@@ -24,6 +24,8 @@ def index(request):
 	return response
 
 def about(request):
+	visitor_cookie_handler(request)
+	
 	if request.session.test_cookie_worked():
 		print("TEST COOKIE WORKED!")
 		request.session.delete_test_cookie()
@@ -31,7 +33,9 @@ def about(request):
 	print(request.method)
 	# prints out the user name, if no one is logged in it prints `AnonymousUser`
 	print(request.user)
-	return render(request, 'rango/about.html', {})
+	context_dict = {}
+	context_dict['visits'] = request.session['visits']
+	return render(request, 'rango/about.html', context_dict)
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass 
